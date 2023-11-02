54d6efcd5265a97a738bb45aa2125264aa419f8c
Tahoor Asma <102382100+tahoorasma@users.noreply.github.com>
2023-11-02 23:49:24 +0500

    project files

---
+++ src/main/java/com/mycompany/quickcartpos/QuickCartPOS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quickcartpos;
+
+/**
+ *
+ * @author Hp
+ */
+public class QuickCartPOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
